e793b186c8940529d542ce87ed394dddad27507e
ali-elmuzayan <ali.elmuzayn@gmail.com>
2025-08-10 18:26:25 +0300

    implement the cabins & about pages

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Link from "next/link";
 import Image from "next/image";
 
-const Logo = () => {
+function Logo() {
     return (
-        <Link href="/" className="flex items-center gap-4 z-10" >
-            <Image src="/logo.png" alt="logo" width={60} height={60} />
-            <span className="text-xl font-semibold text-primary-100">The wild Oasis</span>
+        <Link href="/" className="flex items-center gap-4 z-10">
+            <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
+
+            <span className="text-xl font-semibold text-primary-100">
+        The Wild Oasis
+      </span>
         </Link>
     );
-};
-
+}
 export default Logo;
--- app/_components/Navigation.tsx
+++ app/_components/Navigation.tsx
@@ -1,19 +1,34 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Navigation() {
     return (
         <nav className="z-10 text-xl">
             <ul className="flex gap-16 items-center">
                 <li>
-                    <Link href="/cabins" className="hover:text-accent-400 trnsition-colors">Cabins</Link>
+                    <Link
+                        href="/cabins"
+                        className="hover:text-accent-400 transition-colors"
+                    >
+                        Cabins
+                    </Link>
                 </li>
                 <li>
-                    <Link href="/about" className="hover:text-accent-400 trnsition-colors">About</Link>
+                    <Link
+                        href="/about"
+                        className="hover:text-accent-400 transition-colors"
+                    >
+                        About
+                    </Link>
                 </li>
                 <li>
-                    <Link href="/account" className="hover:text-accent-400 trnsition-colors">Guest area</Link>
+                    <Link
+                        href="/account"
+                        className="hover:text-accent-400 transition-colors"
+                    >
+                        Guest area
+                    </Link>
                 </li>
             </ul>
         </nav>
-    )
+    );
 }
